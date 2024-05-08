13f4f26ce23c0e72acfaba42cfcf9093f532fe56
CHAITANYA BHATIA <117209756+chaitaanyaaaa@users.noreply.github.com>
2024-05-09 02:12:03 +0530

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test()
+    {
+        int a=10;
+        int b=30;
+        int d=10;
+        int e=10;
+        return "just testing  " +(a+b+d+e);
+    }
+}
